172648b1761ddd28f7ac9666b8bf4c36a72cd153
Ivan Cronyn <ivan.cronyn@gmail.com>
2019-08-01 11:22:06 +0100

    Adds help to CLI

--- src/WordscapesCheat.CLI/Program.cs
+++ src/WordscapesCheat.CLI/Program.cs
@@ -8,12 +8,26 @@ using WordscapesCheat;
 namespace WordscapesCheat.CLI
 {
     class Program
-    {        
+    {
         static void Main(string[] args)
         {
-            string characterSet = CheatFunctions.ConvertStringArrayToString(args);
+            // \n is a newline
+            // \t is a tab space
+            const string HelpMessage = "###Wordscapes Cheater###\n\n" + 
+                "Returns matching words based on the characters you provide.\n\n" + 
+                "wsc characters\n\n  characters  The characters that Wordscapes gives you.\n\n" + 
+                "Example:\nwsc cat\nact\ncat";
+
+            if (ArgsNeedsHelp(args))
+            {
+                Console.WriteLine(HelpMessage);
+                return;
+            }
+
+            //We can use the other args for other stuff later
+            //e.g. word count or letter position
+            string characterSet = args[0];
             List<string> dictionary = CheatFunctions.GetDictionary();
-            
             PrintMatchingWords(CheatFunctions.BuildMatchingWordsArray(characterSet, dictionary));
         }
 
@@ -29,8 +43,16 @@ namespace WordscapesCheat.CLI
                 {
                     Console.WriteLine(word);
                 }
-            }            
+            }
         }
 
+        private static bool ArgsNeedsHelp(string[] args)
+        {
+            string[] HelpArgs = new[] { "/?", "--help", "/help", "?" };
+
+            return (args == null || args.Length == 0) //no args at all
+                || args.All(x => string.IsNullOrEmpty(x)) //all args are null or empty string
+                || args.Any(x => HelpArgs.Contains(x)); //any arg contains a help string
+        }
     }
 }
