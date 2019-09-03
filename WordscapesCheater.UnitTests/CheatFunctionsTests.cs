5d0879edcbaaec3554ec6e4d7a0fe1b554671134
Keenan Cronyn <keenan.cronyn@gmail.com>
2019-09-03 20:13:53 +0100

    removes_MSTestframework_andAdds_NUnitFramework

---
+++ WordscapesCheater.UnitTests/CheatFunctionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WordscapesCheat.UnitTests
+{
+    public class CheatFunctionsTests
+    {
+        public void BuildMatchingWordsArray_BadString_ReturnsFalse(string givenLetters, string[] expected)
+        {
+            string givenLetters = "ab-c";
+            List<string> dictionary = CheatFunctions.GetDictionary();
+
+            string[] ArrayResult = CheatFunctions.BuildMatchingWordsArray(givenLetters, dictionary);
+
+            Assert.
+
+        }
+    }
+}
