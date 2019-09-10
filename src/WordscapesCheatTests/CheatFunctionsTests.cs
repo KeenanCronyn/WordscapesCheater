e9241e1756c1f529d3e26dce8627a8f61b4d7d78
Keenan Cronyn <keenan.cronyn@gmail.com>
2019-09-10 14:12:42 +0100

    Add test for bool int array checker and change class inheritance

--- src/WordscapesCheatTests/CheatFunctionsTests.cs
+++ src/WordscapesCheatTests/CheatFunctionsTests.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 namespace WordscapesCheat.Tests
 {
     [TestClass]
-    public class CheatFunctionsTests
+    public class CheatFunctionsTests : CheatFunctions
+        // I derived this class from CheatFunctions because otherwise I could not test the private functions
     {
         [DataTestMethod]
         [DataRow("abc", new int[] { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
@@ -21,22 +22,25 @@ namespace WordscapesCheat.Tests
         }
 
         [DataTestMethod]
-        [DataRow("abc", new int[] { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
-        [DataRow("azaza", new int[] { 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 })]
-        [DataRow("", new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
-        public void BuildMatchingWordsArray_ValidString_Succeed(string input, string[] expectedResult)
+        [DataRow("abc", "cab")]
+        [DataRow("test", "set;stet;test")]
+        public void BuildMatchingWordsArray_ValidString_Succeed(string input, string stringResult)
         {
+            string[] expectedResult = stringResult.Split(';').ToArray();
+
             string[] arrayResult = CheatFunctions.BuildMatchingWordsArray(input, CheatFunctions.GetDictionary());
 
-            CollectionAssert.AreEqual(arrayResult, expectedResult);
+            CollectionAssert.AreEquivalent(arrayResult, expectedResult);
         }
 
-        //[DataTestMethod]
-        //[DataRow("a-c", new int[] { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
-        //public void BuildOccurenceArray_PassInvalidString_Fail(string input, int[] expected)
-        //{
-        //    CollectionAssert.AreEqual(expected, CheatFunctions.BuildOccurenceArray(input));
-        //}
+        [DataTestMethod]
+        [DataRow("abc", "cab")]
+        public void TestIfMatching_PassValidArray_Succeed(string givenLetters, string wordInDictionary)
+        {
+            int[] givenLettersArray = CheatFunctions.BuildOccurenceArray(givenLetters);
+            int[] wordArray = CheatFunctions.BuildOccurenceArray(wordInDictionary);
 
+            Assert.IsTrue(CheatFunctions.TestIfMatching(givenLettersArray, wordArray));
+        }
     }
 }
